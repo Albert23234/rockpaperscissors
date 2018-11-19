a4d2517fdc44e2ceca4109d8586d85879919b1fd
Albert Rondon <hellomyfrend11@hotmail.com>
2018-11-19 23:11:39 +0000

    week 2 RPS project

--- js/script.js
+++ js/script.js
@@ -9,3 +9,23 @@
 
 // DOCUMENT READY FUNCTION BELOW
 
+$("#shoot").click(function(){
+ var userChoice;
+ var userChoice = "";
+var winner = "";
+var computerChoice = "";
+var randomNumber = 0;
+randomNumber= Math.random();
+    console.log(randomNumber);
+    
+ userChoice=$("input").val();
+ $("#userChoice").text(userChoice);
+ if(randomNumber > .50){
+    $("#computerChoice").html("rock");}  
+    else if(randomNumber < .50){
+        $("#computerChoice").html("paper")
+    }
+});
+
+
+    
