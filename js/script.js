6ea3dff55460434abafc08c9756e241cf42ac7b3
Albert Rondon <hellomyfrend11@hotmail.com>
2018-11-26 23:04:48 +0000

    done

--- js/script.js
+++ js/script.js
@@ -20,10 +20,51 @@ randomNumber= Math.random();
     
  userChoice=$("input").val();
  $("#userChoice").text(userChoice);
- if(randomNumber > .50){
-    $("#computerChoice").html("rock");}  
-    else if(randomNumber < .50){
+ if(randomNumber > .3333333333 && randomNumber < .666666666){
+    $("#computerChoice").html("rock");
+     computerChoice = "rock";
+ }  
+    else if(randomNumber < .333333333){
         $("#computerChoice").html("paper")
+        computerChoice = "paper";
+    }
+     else if (randomNumber > .6666666) {
+         $("#computerChoice").html("scissors")
+                 computerChoice = "scissors";
+
+     }
+    if(userChoice === computerChoice ){
+        $("#result").html("draw")
+    }
+    else if(userChoice === "paper" && computerChoice === "rock"){
+        $("#result").html("user Choice wins")
+        
+    }
+ else if (userChoice === "scissors" && computerChoice === "rock"){
+        $("#result").html("computer Choice wins")
+    }
+    else if (userChoice === "scissors" && computerChoice === "paper"){
+        $("#result").html("user Choice wins")}
+    else if (userChoice === "scissors" && computerChoice === "scissors"){
+        $("#result").html("draw")
+    }
+    else if (userChoice === "paper" && computerChoice === "paper"){
+        $("#result").html("draw")
+    }
+    else if (userChoice === "paper" && computerChoice === "scissors"){
+        $("#result").html("computer Choice wins")
+    }
+    else if (userChoice === "rock" && computerChoice === "scissors"){
+        $("#result").html("user Choice wins")
+    }
+    else if (userChoice === "rock" && computerChoice === "rock"){
+        $("#result").html("draw")
+    }
+    else if (userChoice === "rock" && computerChoice === "paper"){
+        $("#result").html("computer Choice wins")
+    } 
+    else{
+        $('#result').html("error")
     }
 });
 
